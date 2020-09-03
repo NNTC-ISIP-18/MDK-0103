455518f4f416788d4904f9535be68a4a07a692fb
Nikitin Ilya <f1uctus@mail.ru>
2020-09-03 23:21:49 +0300

    Rename ids

--- Shmakulator/app/src/main/java/com/example/shmakulator/MainActivity.java
+++ Shmakulator/app/src/main/java/com/example/shmakulator/MainActivity.java
@@ -6,7 +6,6 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 
@@ -29,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
             });
         }
         final MediaPlayer mpClickDel = MediaPlayer.create(this, R.raw.click_del);
-        findViewById(R.id.butdelet).setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.btnDelete).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 mpClickDel.start();
