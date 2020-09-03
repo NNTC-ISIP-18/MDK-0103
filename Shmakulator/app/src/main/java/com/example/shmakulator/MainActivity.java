fc58f879cfaf9d590556f00a561cafbc2469888d
Nikitin Ilya <f1uctus@mail.ru>
2020-09-03 13:23:13 +0300

    Add basic calc layout

--- Shmakulator/app/src/main/java/com/example/shmakulator/MainActivity.java
+++ Shmakulator/app/src/main/java/com/example/shmakulator/MainActivity.java
@@ -2,7 +2,13 @@ package com.example.shmakulator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +16,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final MediaPlayer mp = MediaPlayer.create(this, R.raw.click);
+        ArrayList<View> allButtons = findViewById(R.id.buttonsContainer).getTouchables();
+        for (View v : allButtons) {
+            Button btn = (Button) v;
+            btn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    mp.start();
+                }
+            });
+        }
     }
 }
